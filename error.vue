9e093e73931d69c9ceed143a6e0e861d162727f2
Nico-Battaglia <nico-battaglia@hotmail.com>
2024-09-25 22:24:34 -0300

    Add custom error page

---
+++ error.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex h-screen justify-center items-center flex-col">
+    <h1 class="text-9xl">{{ error.statusCode }}</h1>
+    <p class="mt-7 text-4xl">{{ error.message }}</p>
+    <button @click="handleClick" class="mt-7 rounded bg-blue-600 px-7 py-3 text-xl text-white">
+      Go back
+    </button>
+  </div>
+</template>
+
+<script setup>
+const error = useError()
+
+function handleClick() {
+  navigateTo("/")
+}
+</script>
+
+<style lang="scss" scoped></style>
