d1d980b33ec25d41381b21555c7da766e4080d6f
Nico-Battaglia <nico-battaglia@hotmail.com>
2024-10-16 21:32:36 -0300

    Add googleAuth with supabase

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,9 +3,23 @@
     class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
   >
     <NuxtLink to="/" class="text-3xl">Cartrader</NuxtLink>
+    <div v-if="user" class="flex">
+      <NuxtLink to="/profile/listings" class="mr-5">Profile</NuxtLink>
+      <p class="cursor-pointer" @click="signOut">Logout</p>
+    </div>
+    <NuxtLink v-if="!user" to="/login">Login</NuxtLink>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+
+const signOut = async () => {
+  const { error } = await supabase.auth.signOut()
+  if (error) console.log(error)
+  navigateTo("/")
+}
+</script>
 
 <style lang="css" scoped></style>
---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<script setup>
+definePageMeta({ layout: "custom" })
+
+const supabase = useSupabaseClient()
+
+const user = useSupabaseUser()
+
+const signInWithOAuth = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: "http://localhost:3000",
+    },
+  })
+  if (error) console.log(error)
+}
+</script>
+
+<template>
+  <div class="mt-10">
+    <h1 class="text-5xl font-bold mb-7">Log in</h1>
+    <button @click="signInWithOAuth" class="bg-red-400 p-3 rounded font-bold">
+      Login with Google
+    </button>
+  </div>
+</template>
